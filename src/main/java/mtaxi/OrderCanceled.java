c70d646ac3e3c2a6c3f6b72a8a5b63e85f4572de
eoboon <eoboon@gmail.com>
2020-10-26 21:28:58 +0900

    first commit

---
+++ src/main/java/mtaxi/OrderCanceled.java
@@ -0,0 +1,46 @@
+package mtaxi;
+
+public class OrderCanceled extends AbstractEvent {
+
+    private Long orderId;
+    private Long driverId;
+    private String customerName;
+    private String location;
+    private String status;
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+    public Long getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(Long driverId) {
+        this.driverId = driverId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/mtaxi/OrderStatusRepository.java
@@ -0,0 +1,12 @@
+package mtaxi;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface OrderStatusRepository extends CrudRepository<OrderStatus, Long> {
+
+    List<OrderStatus> findByOrderId(Long orderId);
+
+}
